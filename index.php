f6fda5e11b7e7cba2a071127fddc18c4f71806b3
Masalief Maulana <m.maulana.fb26@gmail.com>
2020-03-02 16:05:15 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Portfolio</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Personal portfolio - Web Developer">
+	<meta name="keywords"
+		content="personal, portfolio, Portofolio, Personal, Personal Portofolio ,creative, Web Developer ">
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="img/logo-new.png" type="image/x-icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/themify-icons.css" />
+	<link rel="stylesheet" href="css/animate.min.css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" />
+	<link rel="stylesheet" href="css/owl.carousel.min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+
+</head>
+
+<!-- <body oncontextmenu="return false;"> -->
+<!-- Aktifkan jika anti inspect elemnt -->
+
+<body>
+	<!-- Preloder -->
+	<div id=" preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section start -->
+	<header class="header-section">
+		<div class="container">
+			<div class="header-warp">
+				<!-- Site Logo -->
+				<a href="#" class="site-logo">
+					<img src="img/logo-new.png" alt="">
+				</a>
+				<!-- Site Menu -->
+				<div class="responsive-switch"><i class="fa fa-bars"></i></div>
+				<nav class="site-menu">
+					<div class="sm-close"><i class="ti-close"></i></div>
+					<ul class="menu-list">
+						<li><a href="#home">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#resume">Resume</a></li>
+						<li><a href="#works">Works</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+	<!-- Hero section start -->
+	<section class="hero-section hero-style-three" id="home">
+		<div id="bubble"></div>
+		<div class="typed-area">
+			<div class="container">
+				<!-- typed text -->
+				<h2><span id="typed-text"></span></h2>
+			</div>
+		</div>
+		<figure class="shap-image">
+			<img src="img/hero-shap.png" alt="">
+		</figure>
+	</section>
+	<!-- Hero section end -->
+
+	<!-- About section start -->
+	<section class="about-section spad" id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+					<figure>
+						<img src="/img/picture.png" alt="Profile">
+					</figure>
+				</div>
+				<div class="col-lg-6 about-text wow fadeInDown" data-wow-delay="0.5s">
+					<div class="about-title">
+						<h2>Introduce Myself</h2>
+						<h5>Who I am</h5>
+					</div>
+					<p>I am <span>Masalief Maulana</span>
+						. I'm a student at SMK 17 Agustus 1945. I was born and live on Surabaya.
+						I also work in software companies in Surabaya as an web developer.</p>
+					<div class="skills mt-5">
+						<!-- Skill item -->
+						<div class="single-progress-item">
+							<p>Analytic</p>
+							<div class="progress-bar-style" data-progress="87"></div>
+						</div>
+						<!-- Skill item -->
+						<div class="single-progress-item">
+							<p>Design</p>
+							<div class="progress-bar-style" data-progress="70"></div>
+						</div>
+						<!-- Skill item -->
+						<div class="single-progress-item">
+							<p>Coding</p>
+							<div class="progress-bar-style" data-progress="78"></div>
+						</div>
+						<!-- Skill item -->
+						<div class="single-progress-item">
+							<p>Leadership</p>
+							<div class="progress-bar-style" data-progress="80"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- services -->
+			<div class="service-slider owl-carousel spad pb-0">
+				<!-- item -->
+				<div class="service">
+					<i class="ti-settings"></i>
+					<div class="service-text">
+						<h3>Development</h3>
+						<p>Work efficiently and effectively to make good quality from our company</p>
+					</div>
+				</div>
+				<!-- item -->
+				<div class="service">
+					<i class="ti-medall"></i>
+					<div class="service-text">
+						<h3>Best</h3>
+						<p>work with the best coder to lift higher quality and quantity</p>
+					</div>
+				</div>
+				<!-- item -->
+				<div class="service">
+					<i class="ti-heart"></i>
+					<div class="service-text">
+						<h3>Love</h3>
+						<p>build with love and care from us</p>
+					</div>
+				</div>
+				<!-- item -->
+				<div class="service">
+					<i class="ti-key"></i>
+					<div class="service-text">
+						<h3>Security</h3>
+						<p>The best and most trusted protection to keep your database safety</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- About section end -->
+
+	<!-- Resume section start -->
+	<section class="resume-section spad" id="resume">
+		<div class="container">
+			<div class="section-title">
+				<p>Resume</p>
+			</div>
+			<div class="row">
+				<div class="col-lg-10 col-xl-9 offset-lg-2 offset-xl-2">
+					<!-- Review item
+					<div class="resume-item wow fadeInUp" data-wow-delay="0.2s">
+						<div class="resume-year">
+							<h2>2017</h2>
+						</div>
+						<div class="resume-text">
+							<h3>First Learn Coding</h3>
+							<p>Until now</p>
+						</div>
+					</div>
+					Review item
+					<div class="resume-item wow fadeInUp" data-wow-delay="0.4s">
+						<div class="resume-year">
+							<h2>2018</h2>
+						</div>
+						<div class="resume-text">
+							<h3>First School in SMK 17 Agustus 1945</h3>
+							<p>Until now</p>
+						</div>
+					</div>
+					Review item -->
+					<div class="resume-item wow fadeInUp" data-wow-delay="0.2s">
+						<div class="resume-year">
+							<h2>2019</h2>
+						</div>
+						<div class="resume-text">
+							<h3>Working as Web Developer on Quantum Leap</h3>
+							<p>Until now</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Resume section end -->
+
+	<!-- Portfolio section start -->
+	<section class="portfolio-section spad" id="works">
+		<div class="container">
+			<div class="section-title">
+				<p>Works</p>
+				<h2>Letest Projects soon updated</h2>
+			</div>
+			<div class="text-center mt-5">
+				<a href="#" class="site-btn wow fadeInUp" data-wow-delay="0.2s">More Projects</a>
+			</div>
+		</div>
+	</section>
+	<!-- Portfolio section end -->
+
+	<!-- Contact section start -->
+	<section class="contact-section spad" id="contact">
+		<div class="container">
+			<div class="section-title">
+				<p>Contact</p>
+				<h2>Get in touch</h2>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2">
+					<div class="row">
+						<!-- contact info -->
+						<div class="col-sm-4 cont-info wow fadeInUp" data-wow-delay="0.2s">
+							<i class="ti-email"></i>
+							<a href="mailto:viole.margaretha@gmail.com">admin@viole.website</a>
+							<p>email</p>
+						</div>
+						<!-- contact info -->
+						<div class="col-sm-4 cont-info wow fadeInUp" data-wow-delay="0.4s">
+							<i class="ti-location-pin"></i>
+							<span>Bratang Gede 1 Tangkis no 36, Surabaya, ID </span>
+							<p>Address</p>
+						</div>
+						<!-- contact info -->
+						<div class="col-sm-4 cont-info wow fadeInUp" data-wow-delay="0.6s">
+							<i class="ti-mobile"></i>
+							<span>+62822 3424 1986</span>
+							<p>Phone</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Contact section end -->
+
+	<!-- Footer section start -->
+	<footer class="footer-section spad set-bg" data-setbg="img/footer-bg.svg">
+		<div class="container">
+			<div class="social-links">
+				<a href="https://facebook.com/Viole403" target="_blank" class="facebook wow fadeInDown"
+					data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a>
+				<a href="https://github.com/Viole403" target="_blank" class="facebook wow fadeInDown"
+					data-wow-delay="0.4s"><i class="fa fa-github"></i></a>
+				<a href="https://t.me/Viole403" target="_blank" class="facebook wow fadeInDown" data-wow-delay="0.6s"><i
+						class="fa fa-telegram"></i></a>
+			</div>
+			<!-- copyright -->
+			<p class="copyright">Copyright &copy; 2020, All rights Reserved.<br>Created by Maulana</p>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+
+	<!-- Jquery & Javascripts files  -->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/jquery.nav.min.js"></script>
+	<script src="js/isotope.pkgd.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/typed.min.js"></script>
+	<script src="js/scripts.js"></script>
+
+	<!-- For bubble style -->
+	<script src="js/bubble.js"></script>
+	<script src="js/init-bubble.js"></script>
+
+	<!-- For Inspect-->
+	<!-- <script src="js/blocker-inspect.js"></script> -->
+	<!-- aktfkan jika ingin menggunakan anti inspect -->
+
+	<!-- End Body Anti Inspect Elemet -->
+	<!-- </body oncontextmenu="return false;"> -->
+</body>
+
+</html>
